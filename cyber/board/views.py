7d2d59a79af7203e8a4bf16fad392b0704596bb5
ffx01 <walters.justin01@gmail.com>
2016-03-16 22:44:37 -0700

    V0.0.1 - Enabled creation of new threads for single board view

--- cyber/board/views.py
+++ cyber/board/views.py
@@ -1,18 +1,53 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.views.generic import View
 
 from .models import Board
+from thread.models import Thread
+from post.models import Post
 
 
 class SingleBoard(View):
 
     def get(self, request, slug):
+
         board = get_object_or_404(Board, slug=slug)
+
         context = {
-            'board': board,
+            'board': board
         }
+
         return render(
             request,
             'board/single_board.html',
             context,
+        )
+
+    def post(self, request, slug):
+
+        board = get_object_or_404(Board, slug=slug)
+
+        new_thread = Thread(
+            title=request.POST['thread_title'],
+            board=board
+        )
+
+        new_thread.save()
+
+        initial_post = Post(
+            image=request.FILES['image'],
+            body=request.POST['body'],
+            parent_thread=Thread.objects.get(id=new_thread.id)
+        )
+
+        initial_post.save()
+
+        return HttpResponseRedirect(
+            reverse(
+                'thread:single_thread',
+                kwargs={
+                    'id': Thread.objects.get(id=new_thread.id).id
+                }
+            )
         )
